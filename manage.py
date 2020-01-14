553384676aa2dda99d5e8909e9265b40818e6fdb
zdl <63518105@qq.com>
2020-01-14 09:28:40 +0800

    zdl

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ from config import DevelopmentConfig as Config
 from application import create_app
 app = create_app(Config)
 manager = Manager(app)
-# 运行本地服务
 manager.add_command("runserver", Server(app.config["HOST"], port=app.config["PORT"], passthrough_errors=True))
 
 if __name__ == "__main__":
